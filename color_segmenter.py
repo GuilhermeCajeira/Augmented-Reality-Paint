d14a0c9fc9658bfeebde2bd27e91383904c1b19c
Guilherme Cajeira <gcajeira@ua.pt>
2022-10-27 13:48:35 +0100

    Update color_segment.py

--- color_segmenter.py
+++ color_segmenter.py
@@ -10,19 +10,7 @@ import json
 def onTrackbar(value, channel, min_max, window_name, image, limits):
     limits[channel][min_max] = value  # update corresponding value in limits dict
 
-    # Processing
-    mins = np.array([limits['B']['min'], limits['G']['min'], limits['R']['min']])
-    maxs = np.array([limits['B']['max'], limits['G']['max'], limits['R']['max']])
-    image_mask = cv2.inRange(image, mins, maxs)
-    image_mask = image_mask.astype(bool)  # conversion from numpy from uint8 to bool
-
-    image_processed = copy.deepcopy(image)
-    image_processed[np.logical_not(image_mask)] = 0
-
     print('Selected threshold ' + str(value) + ' for limit ' + str(min_max) + ' ' + str(channel))
-
-    cv2.imshow(window_name, image_processed)
-
 
 # def mouseCallback(event, x, y, flags, param):
 #     if event == cv2.EVENT_LBUTTONDOWN:
@@ -39,9 +27,18 @@ def main():
 
     cv2.namedWindow(window_name_original,cv2.WINDOW_AUTOSIZE)
 
-    # path_to_classifier = '/home/guilherme/UA/PSR/PSR/Parte7/opencv-4.6.0/data/haarcascades/'
-    # face_cascade = cv2.CascadeClassifier(path_to_classifier + 'haarcascade_frontalface_default.xml')
+    _, image = capture.read()  # get an image from the camera
 
+    limits = {'B': {'min': 0, 'max': 256},
+              'G': {'min': 0, 'max': 256},
+              'R': {'min': 0, 'max': 256}}
+
+    cv2.createTrackbar('MinB', window_name_original, 0,   256, partial(onTrackbar, channel='B', min_max='min',window_name=window_name_segmented, image=image, limits=limits))
+    cv2.createTrackbar('MaxB', window_name_original, 256, 256, partial(onTrackbar, channel='B', min_max='max',window_name=window_name_segmented, image=image, limits=limits))
+    cv2.createTrackbar('MinG', window_name_original, 0,   256, partial(onTrackbar, channel='G', min_max='min',window_name=window_name_segmented, image=image, limits=limits))
+    cv2.createTrackbar('MaxG', window_name_original, 256, 256, partial(onTrackbar, channel='G', min_max='max',window_name=window_name_segmented, image=image, limits=limits))
+    cv2.createTrackbar('MinR', window_name_original, 0,   256, partial(onTrackbar, channel='R', min_max='min',window_name=window_name_segmented, image=image, limits=limits))
+    cv2.createTrackbar('MaxR', window_name_original, 256, 256, partial(onTrackbar, channel='R', min_max='max',window_name=window_name_segmented, image=image, limits=limits))
 
     # -----------------------------------------------
     # EXECUTION
@@ -53,35 +50,24 @@ def main():
             print('Video is over, terminating')
             break                   # video is over
 
-        image_GUI = copy.deepcopy(image)
+        # image_GUI = copy.deepcopy(image)
 
-        # -----------------------------------
-        # Face Detection
-        # -----------------------------------
-        # Convert to grayscale
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Processing
+        mins = np.array([limits['B']['min'], limits['G']['min'], limits['R']['min']])
+        maxs = np.array([limits['B']['max'], limits['G']['max'], limits['R']['max']])
+        image_mask = cv2.inRange(image, mins, maxs)
+        image_mask = image_mask.astype(bool)  # conversion from numpy from uint8 to bool
 
-        # Detect the faces
-        # faces = face_cascade.detectMultiScale(image_gray, 1.1, 4)
+        image_processed = copy.deepcopy(image)
+        image_processed[np.logical_not(image_mask)] = 0
+
         cv2.imshow(window_name_original, image) 
-        cv2.imshow(window_name_segmented, image) 
-        
-
-        limits = {'B': {'min': 0, 'max': 256},
-                  'G': {'min': 0, 'max': 256},
-                  'R': {'min': 0, 'max': 256}}
-
-        cv2.createTrackbar('MinB', window_name_original, 0,   256, partial(onTrackbar, channel='B', min_max='min',window_name=window_name_segmented, image=image, limits=limits))
-        cv2.createTrackbar('MaxB', window_name_original, 256, 256, partial(onTrackbar, channel='B', min_max='max',window_name=window_name_segmented, image=image, limits=limits))
-        cv2.createTrackbar('MinG', window_name_original, 0,   256, partial(onTrackbar, channel='G', min_max='min',window_name=window_name_segmented, image=image, limits=limits))
-        cv2.createTrackbar('MaxG', window_name_original, 256, 256, partial(onTrackbar, channel='G', min_max='max',window_name=window_name_segmented, image=image, limits=limits))
-        cv2.createTrackbar('MinR', window_name_original, 0,   256, partial(onTrackbar, channel='R', min_max='min',window_name=window_name_segmented, image=image, limits=limits))
-        cv2.createTrackbar('MaxR', window_name_original, 256, 256, partial(onTrackbar, channel='R', min_max='max',window_name=window_name_segmented, image=image, limits=limits))
-
+        cv2.imshow(window_name_segmented, image_processed) 
         
         # cv2.setMouseCallback(window_name_original, mouseCallback)
+        # print('limts = ' + str(limits))
 
-        pressed_key = cv2.waitKey()
+        pressed_key = cv2.waitKey(20)
         if chr(pressed_key) == 'q': # Quite the program
             exit(0)
         elif chr(pressed_key) == 'w': 
